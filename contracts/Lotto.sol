4125069a6d8349466fd795e9552f6f54f927e13c
Bryan Mytko <bryanmytko@gmail.com>
2017-06-09 18:30:10 -0400

    Add basic constructor

--- contracts/Lotto.sol
+++ contracts/Lotto.sol
@@ -1,3 +1,16 @@
+pragma solidity ^0.4.11;
+
 contract Lotto {
+  address public owner;
+  uint ticket_price;
+
+  function Lotto(){
+    owner = msg.sender;
+  }
+
+  function buy_ticket public payable(){
+    // Keyword `payable` allows ether to be sent
+    // Do something with msg.sender, msg.value
+  }
 
 }
