0926261fe88073f8556614d6075a4c0ad1c49753
Bryan Mytko <bryanmytko@gmail.com>
2017-06-11 02:56:15 -0400

    Add buy ticket functionality

--- contracts/Lotto.sol
+++ contracts/Lotto.sol
@@ -2,15 +2,36 @@ pragma solidity ^0.4.11;
 
 contract Lotto {
   address public owner;
-  uint ticket_price;
+  address[] public participants;
+  address public winner;
+
+  uint prize_total = 10 ether;
+  uint prize_tally = 0;
+  uint ticket_price = 1 ether;
+
+  event ticket_bought(
+    uint _prize_tally
+  );
 
   function Lotto(){
     owner = msg.sender;
   }
 
-  function buy_ticket public payable(){
-    // Keyword `payable` allows ether to be sent
-    // Do something with msg.sender, msg.value
+  function buy_ticket() public payable {
+    if(prize_tally < prize_total){
+      participants.push(msg.sender);
+      prize_tally += ticket_price;
+
+      ticket_bought(prize_tally);
+    }
+  }
+
+  function get_participants() constant public returns(address[]){
+    return participants;
+  }
+
+  function get_tally() constant public returns(uint){
+    return prize_tally;
   }
 
 }
