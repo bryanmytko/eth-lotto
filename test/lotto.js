7f74837b349e1870a6d46d739b9f054a23b60a29
Bryan Mytko <bryanmytko@gmail.com>
2017-06-11 02:56:35 -0400

    Add non-working test -- need to learn this

--- test/lotto.js
+++ test/lotto.js
@@ -1,5 +1,11 @@
 var Lotto = artifacts.require("./Lotto.sol");
 
 contract('Lotto', function(accounts) {
-  // @TODO
+  it("should purchase a ticket if tickets are available", function(){
+    return Lotto.deployed().then(function(instance){
+      return instance.get_tally.call();
+    }).then(function(tally){
+      assert.equal(tally.valueOf(), 1, "Incorrect value");
+    });
+  });
 });
